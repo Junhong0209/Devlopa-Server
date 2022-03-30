334bc65a4896c5a089d7cef926f5675df18aba4b
Junhong0209 <junh040209@naver.com>
2022-03-30 11:13:23 +0900

    :sparkles: :: Databse Model Settings
    Django의 기본 모델 커스텀 및  User 모델 추가

--- v1/models.py
+++ v1/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+  use_in_migrations = True
+  
+  def _create_user(self, username, unique_id, grade, room, number, profile_image=None, **extra_fields):
+    if not unique_id:
+      raise ValueError('No unique id.')
+    
+    user = self.model(unique_id=unique_id, grade=grade, room=room, number=number, username=username, profile_image=profile_image, **extra_fields)
+    user.save(using=self.db)
+    
+    return user
+    
+  def create_user(self, username, unique_id, grade, room, number, profile_image=None, **extra_fields):
+    extra_fields.setdefault('is_staff', False)
+    extra_fields.setdefault('is_superuser', False)
+    
+    return self._create_user(username, unique_id, grade, room, number, profile_image, **extra_fields)
+
+
+class User(AbstractUser):
+  object = UserManager()
+  
+  unique_id = models.CharField(verbose_name='pk', db_column='pk', primary_key='True', max_length=255, null=False, unique=True)
+  username = models.CharField(verbose_name='user_name', db_column='user_name', max_length=10)
+  grade = models.IntegerField(verbose_name='grade', db_column='grade')
+  room = models.IntegerField(verbose_name='room', db_column='room')
+  number = models.IntegerField(verbose_name='number', db_column='number')
+  profile_image = models.CharField(verbose_name='profile_image', db_column='profile_image', max_length=255, null=True)
+  
+  USERNAME_FIELD = 'unique_id'

d641a6769bd11aafa6e2fbad3720d316cea22d62
Junhong0209 <junh040209@naver.com>
2022-04-28 14:53:27 +0900

    :bug: :: 토큰 조회 오류 및 게시글 수정 오류 수정

--- v1/views.py
+++ v1/views.py
@@ -53,8 +53,7 @@ class GetDodamUser(APIView):
       if TypeError:
         return Response(
           status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-          data=CUSTOM_CODE(
-            status=500, message='값을 가져오는 도중 문제가 생겼습니다.')
+          data=CUSTOM_CODE(status=500, message='값을 가져오는 도중 문제가 생겼습니다.')
         )
       else:
         return Response(
@@ -83,14 +82,12 @@ class GetDodamUser(APIView):
     }
     
     try:
-      response = requests.get(os.environ.get(
-        'DODAM_USER_API'), headers=header)
+      response = requests.get(os.environ.get('DODAM_USER_API'), headers=header)
       user_data = response.json()['data']
     except (KeyError, ValueError):
       return Response(
         status=status.HTTP_400_BAD_REQUEST,
-        data=CUSTOM_CODE(
-          status=400, message='Some Values are missing.')
+        data=BAD_REQUEST_400(message='Some Values are missing.')
       )
     
     try:
@@ -113,15 +110,20 @@ class GetDodamUser(APIView):
     
     return Response(
       status=status.HTTP_200_OK,
-      data=OK_200(message='도담의 유저 정보를 성공적으로 불러와 저장했습니다.',
-                  data={'token': token.key})
+      data=OK_200(message='도담의 유저 정보를 성공적으로 불러와 저장했습니다.', data={'token': token.key})
     )
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserPosting(APIView):
   def post(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
@@ -155,41 +157,90 @@ class UserPosting(APIView):
     )
   
   def get(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
       )
     
     try:
-      posting_objects = Post.objects.all()
-    except (KeyError, ValueError):
-      return Response(
-        status=status.HTTP_400_BAD_REQUEST,
-        data=BAD_REQUEST_400(message='Some Values are missing.')
-      )
+      if request.GET['post_idx']:
+        try:
+          posting_object = Post.objects.get(primaryKey=request.GET['post_idx'], user_id=request.user)
+
+          data = {
+            'contents': {
+              'idx': posting_object.primaryKey,
+              'user_name': posting_object.user.username,
+              'profile_image': posting_object.user.profile_image,
+              'grade': posting_object.user.grade,
+              'room': posting_object.user.room,
+              'number': posting_object.user.number,
+              'content': posting_object.content,
+              'write_time': posting_object.writeTime.strftime('%Y-%m%d %H:%M:%S')
+            }
+          }
+
+          return Response(
+            status=status.HTTP_200_OK,
+            data=OK_200(message='게시글 조회를 성공했습니다.', data=data)
+          )
+        except (KeyError, ValueError):
+          return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data=BAD_REQUEST_400(message='Some Values are missing.')
+          )
+        
+    except MultiValueDictKeyError:
+      try:
+        posting_objects = Post.objects.all()
+        
+        user_profile = {
+          'profile_image': request.user.profile_image,
+          'user_name': request.user.username,
+          'grade': request.user.grade,
+          'room': request.user.room,
+          'number': request.user.number
+        }
     
-    user_profile = {
-      'profile_image': request.user.profile_image,
-      'user_name': request.user.username,
-      'grade': request.user.grade,
-      'room': request.user.room,
-      'number': request.user.number
-    }
-    
-    data = PostingObject(posting_objects)
-    data['user_profile'] = user_profile
-    
-    return Response(
-      status=status.HTTP_200_OK,
-      data=OK_200(message='전체 글 조회를 성공했습니다.', data=data)
-    )
+        data = PostingObject(posting_objects)
+        data['user_profile'] = user_profile
+  
+        return Response(
+          status=status.HTTP_200_OK,
+          data=OK_200(message='전체 글 조회를 성공했습니다.', data=data)
+        )
+      except (KeyError, ValueError):
+        return Response(
+          status=status.HTTP_400_BAD_REQUEST,
+          data=BAD_REQUEST_400(message='Some Values are missing.')
+        )
   
   def put(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+      )
+    
+    try:
+      post_data = Post.objects.get(primaryKey=request.data['post_idx'])
+    except ObjectDoesNotExist:
+      return Response(
+        status=status.HTTP_400_BAD_REQUEST,
+        data=BAD_REQUEST_400(message='존재하지 않는 게시글입니다.')
       )
     
     try:
@@ -200,21 +251,33 @@ class UserPosting(APIView):
         data=BAD_REQUEST_400(message='Some Values are missing.')
       )
     
-    Post.content = content
-    Post.save()
-    
-    return Response(
-      status=status.HTTP_200_OK,
-      data=OK_200(message='글이 성공적으로 수정되었습니다.')
-    )
+    if post_data.user.unique_id == request.user:
+      post_data.content = content
+      post_data.save()
+  
+      return Response(
+        status=status.HTTP_200_OK,
+        data=OK_200(message='게시글이 성공적으로 수정되었습니다.')
+      )
+    else:
+      return Response(
+        status=status.HTTP_401_UNAUTHORIZED,
+        data=INVALID_TOKEN(message='사용자의 게시글이 아닙니다.')
+      )
   
   def delete(self, request):
-    if not request.user.is_authenticatend or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
       )
-    
+  
     try:
       post_id = request.data['post_idx']
     except (KeyError, ValueError):
@@ -222,32 +285,31 @@ class UserPosting(APIView):
         status=status.HTTP_400_BAD_REQUEST,
         data=BAD_REQUEST_400(message='게시글 idx가 존재하지 않습니다.')
       )
-    
+  
     try:
-      post_data = Post.objects.get(primaryKey=post_id)
-    except ObjectDoesNotExist:
-      return Response(
-        status=status.HTTP_400_BAD_REQUEST,
-        data=BAD_REQUEST_400(message='존재하지 않는 댓글입니다.')
-      )
-    
-    if post_data.user.unique_id == request.user:
+      post_data = Post.objects.get(primaryKey=post_id, user_id=request.user)
       post_data.delete()
       return Response(
         status=status.HTTP_200_OK,
         data=OK_200(message='게시글이 성공적으로 삭제되었습니다.')
       )
-    else:
+    except ObjectDoesNotExist:
       return Response(
-        status=status.HTTP_401_UNAUTHORIZED,
-        data=INVALID_TOKEN(message='사용자의 게시글이 아닙니다.')
+        status=status.HTTP_400_BAD_REQUEST,
+        data=BAD_REQUEST_400(message='본인의 게시글이 아닙니다.')
       )
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserProfile(APIView):
   def get(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
@@ -263,25 +325,31 @@ class UserProfile(APIView):
     
     try:
       posting_objects = Post.objects.filter(user_id=request.user)
+    
+      post_data = PostingObject(posting_objects)
+      post_data['user_profile'] = user_profile
+      
+      return Response(
+        status=status.HTTP_200_OK,
+        data=OK_200(message='유저 프로필 조회를 성공했습니다.', data=post_data)
+      )
     except ObjectDoesNotExist:
       return Response(
         status=status.HTTP_400_BAD_REQUEST,
         data=BAD_REQUEST_400(message='유저 데이터가 존재하지 않습니다.')
       )
-    
-    post_data = PostingObject(posting_objects)
-    post_data['user_profile'] = user_profile
-    
-    return Response(
-      status=status.HTTP_200_OK,
-      data=OK_200(message='유저 프로필 조회를 성공했습니다.', data=post_data)
-    )
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class PostComment(APIView):
   def post(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
@@ -297,25 +365,31 @@ class PostComment(APIView):
     
     try:
       comment_data = request.data['comment']
+    
+      comment = Comment(
+        post_id=post_idx,
+        comment=comment_data,
+      )
+      comment.save()
+      
+      return Response(
+        status=status.HTTP_200_OK,
+        data=OK_200(message='댓글이 성공적으로 작성되었습니다.')
+      )
     except (KeyError, ValueError):
       return Response(
         status=status.HTTP_400_BAD_REQUEST,
         data=BAD_REQUEST_400(message='댓글이 작성 되지 않았습니다.')
       )
-    
-    comment = Comment(
-      post_id=post_idx,
-      comment=comment_data,
-    )
-    comment.save()
-    
-    return Response(
-      status=status.HTTP_200_OK,
-      data=OK_200(message='댓글이 성공적으로 작성되었습니다.')
-    )
   
   def get(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
@@ -326,26 +400,32 @@ class PostComment(APIView):
     except MultiValueDictKeyError:
       return Response(
         status=status.HTTP_400_BAD_REQUEST,
-        data=BAD_REQUEST_400(message='params에 게시글 idx가 존재 하지 않습니다.')
+        data=BAD_REQUEST_400(message='params에 게시글 idx가 존재하지 않습니다.')
       )
     
     try:
       comment_objects = Comment.objects.filter(post_id=post_idx)
+    
+      comment_data = CommentObject(comment_objects)
+      
+      return Response(
+        status=status.HTTP_200_OK,
+        data=OK_200(message='댓글을 성공적으로 불러왔습니다.', data=comment_data)
+      )
     except ObjectDoesNotExist:
       return Response(
         status=status.HTTP_200_OK,
         data=OK_200(message='게시글의 댓글이 존재하지 않습니다.')
       )
-    
-    comment_data = CommentObject(comment_objects)
-    
-    return Response(
-      status=status.HTTP_200_OK,
-      data=OK_200(message='댓글을 성공적으로 불러왔습니다.', data=comment_data)
-    )
   
   def put(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
@@ -364,7 +444,7 @@ class PostComment(APIView):
     except (KeyError, ValueError):
       return Response(
         status=status.HTTP_400_BAD_REQUEST,
-        data=BAD_REQUEST_400(message='게시글 idx가 존재하지 않습니다.')
+        data=BAD_REQUEST_400(message='댓글 idx가 존재하지 않습니다.')
       )
     
     try:
@@ -390,7 +470,13 @@ class PostComment(APIView):
       )
   
   def delete(self, request):
-    if not request.user.is_authenticated or request.user.is_anonymous:
+    try:
+      if not request.user.is_authenticated or request.user.is_anonymous:
+        return Response(
+          status=status.HTTP_401_UNAUTHORIZED,
+          data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')
+        )
+    except AttributeError:
       return Response(
         status=status.HTTP_401_UNAUTHORIZED,
         data=INVALID_TOKEN(message='토큰이 존재하지 않습니다.')

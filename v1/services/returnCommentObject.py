8b7b29e4831a21bbfd18fec9a6945df288961e9e
Junhong0209 <junh040209@naver.com>
2022-05-11 09:03:05 +0900

    :art: :: Modify returnCommentObject.py
    return 값에 유저 프로필 이미지, 작성 시간 제거

--- v1/services/returnCommentObject.py
+++ v1/services/returnCommentObject.py
@@ -1,15 +1,13 @@
 def CommentObject(value):
   data = {'list_count': 0, 'contents': []}
   
-  comment_objects = value.order_by('writeTime')
+  comment_objects = value.order_by('write_time')
   comment_objects = list(comment_objects)
   for comment_object in comment_objects:
     comment_data = {
       'idx': comment_object.primary_key,
       'user_name': comment_object.user.username,
-      'profile_image': comment_object.user.profile_image,
       'content': comment_object.comment,
-      'write_time': comment_object.write_time.strftime('%Y-%m-%d %H:%M:%S')
     }
     data['list_count'] += 1
     data['contents'].append(comment_data)
